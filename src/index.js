f60d5170f667105206319b2fde7999610ec60c12
Greg Barnes <gregdbarnes@gmail.com>
2018-10-09 14:55:38 +0100

    Implement optimistic drag and drop for basic list, no api calls

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@atlaskit/css-reset";
+import { DragDropContext } from "react-beautiful-dnd";
+import initialData from "./initial-data";
+import Section from "./Section";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// fetch("http://localhost:59022/Apply/Sequences").then(result => {
+//   console.log(result);
+// });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  state = initialData;
+
+  onDragEnd = result => {
+    const { destination, source, draggableId } = result;
+
+    // If dropped outside droppable zone (destination === null)
+    if (!destination) return;
+    // If dropped in the same place as picked up
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    const section = this.state.sections[source.droppableId];
+    const newQuestionIds = Array.from(section.questionIds);
+    newQuestionIds.splice(source.index, 1);
+    newQuestionIds.splice(destination.index, 0, draggableId);
+
+    const newSection = {
+      ...section,
+      questionIds: newQuestionIds
+    };
+
+    const newState = {
+      ...this.state,
+      sections: {
+        ...this.state.sections,
+        [newSection.id]: newSection
+      }
+    };
+
+    this.setState(newState);
+  };
+
+  render() {
+    return (
+      <DragDropContext
+        // onDragStart
+        // onDragUpdate
+        // onDragEnd //required
+
+        onDragEnd={this.onDragEnd}
+      >
+        {this.state.sectionOrder.map(sectionId => {
+          const section = this.state.sections[sectionId];
+          const questions = section.questionIds.map(
+            questionId => this.state.questions[questionId]
+          );
+
+          return (
+            <Section key={sectionId} section={section} questions={questions} />
+          );
+        })}
+      </DragDropContext>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
